11490e1376ffc06fa527ca20c8d8a9f6e0a40f27
unknown <gabriel.silvagomes2006@gmail.com>
2025-09-22 18:02:30 -0300

    Criado card de produto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { HeaderComponent } from './shared/header/header.component';
 import { NavComponent } from './shared/nav/nav.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { ProdCadastroComponent } from './pages/prod-cadastro/prod-cadastro.component';
+import { ProdAddComponent } from './pages/prod-add/prod-add.component';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, LandingPageComponent, HeaderComponent, 
-    NavComponent, FooterComponent, ProdCadastroComponent],
+    NavComponent, FooterComponent, ProdCadastroComponent, ProdAddComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

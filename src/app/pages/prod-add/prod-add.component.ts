faf4001aa031ba5dcca590c90bed5535ff3d9593
unknown <gabriel.silvagomes2006@gmail.com>
2025-09-24 13:40:46 -0300

    Adicao de campos editavies

--- src/app/pages/prod-add/prod-add.component.ts
+++ src/app/pages/prod-add/prod-add.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-prod-add',
@@ -6,6 +6,32 @@ import { Component } from '@angular/core';
   templateUrl: './prod-add.component.html',
   styleUrl: './prod-add.component.css'
 })
-export class ProdAddComponent {
+export class ProdAddComponent implements AfterViewInit {
 
+  ngAfterViewInit(): void {
+    const inputImg = document.getElementById("item-image") as HTMLInputElement;
+    const uploadArea = document.getElementById("upload-area") as HTMLElement;
+
+    inputImg?.addEventListener("change", () => {
+      const file = inputImg.files?.[0];
+      if (file) {
+        const reader = new FileReader();
+        reader.onload = e => {
+          const imageUrl = e.target?.result as string;
+
+          // Define a imagem como fundo da área
+          uploadArea.style.backgroundImage = `url('${imageUrl}')`;
+          uploadArea.classList.add("has-image");
+        };
+        reader.readAsDataURL(file);
+      }
+    });
+
+    const sizeLabels = document.querySelectorAll(".item-sizes label");
+    sizeLabels.forEach(label => {
+      label.addEventListener("click", () => {
+        label.classList.toggle("active");
+      });
+    });
+  }
 }

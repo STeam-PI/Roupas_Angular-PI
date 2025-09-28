796e9c59f31d2ad14f702cbbfd4d1e9ff2d67019
Gabriel Fausto <gabriel.dota16@gmail.com>
2025-09-28 20:39:52 -0300

    ajustes de layout e rota

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavComponent } from '../nav/nav.component';
 import { RouterModule } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -9,5 +10,29 @@ import { RouterModule } from '@angular/router';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  constructor(private router: Router) {
+    router.events.subscribe((val) => {
+      if (val instanceof NavigationEnd) {
+        if (val.url === '/login') {
+          for (let element of Array.from(document.getElementsByClassName('header__dinamico'))) {
+            element.classList.add('hidden');
+          }
+        }
+        else {
+          for (let element of Array.from(document.getElementsByClassName('header__dinamico'))) {
+            element.classList.remove('hidden');
+          }
+        }
+      }
+    });
+  }
 
+  toggleMenu() {
+    const nav = document.getElementById('headerNav');
+    nav?.classList.toggle('menu-open');
+  }
+
+  goHome() {
+    this.router.navigate(['']);
+  }
 }

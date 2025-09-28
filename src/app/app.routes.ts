c7beb06b52c73403555909635a9c824c931c32b2
Gabriel Fausto <gabriel.dota16@gmail.com>
2025-09-28 18:17:02 -0300

    ajustes de rota e formulario no login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { LoginComponent } from './pages/login/login.component';
 import { ProdAddComponent } from './pages/prod-add/prod-add.component';
+import { ProdCadastroComponent } from './pages/prod-cadastro/prod-cadastro.component';
 
 export const routes: Routes = [
-  {path:'', component: LandingPageComponent},
-  {path:'login', component: LoginComponent},
-  {path:'roupas/cadastro', component: ProdAddComponent},
-  {path:'roupas/listagem', component: ProdAddComponent},
+  { path: '', component: LandingPageComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'roupas/cadastro', component: ProdAddComponent },
+  { path: 'roupas/listagem', component: ProdCadastroComponent },
 ];

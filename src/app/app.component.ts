8b54689c4d8ff17756cbb4a35a68c2bd031380a2
Gabriel Fausto <gabriel.dota16@gmail.com>
2025-09-30 18:59:36 -0300

    melhoria na responsividade do footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { HeaderComponent } from './shared/header/header.component';
-import { NavComponent } from './shared/nav/nav.component';
 import { FooterComponent } from './shared/footer/footer.component';
-import { DadosPessoaisComponent } from './pages/dados-pessoais/dados-pessoais.component';
-import { LoginComponent } from './pages/login/login.component';
-import { ProdCadastroComponent } from './pages/prod-cadastro/prod-cadastro.component';
-import { ProdAddComponent } from './pages/prod-add/prod-add.component';
 
 @Component({
   selector: 'app-root',
-  imports: [LoginComponent, DadosPessoaisComponent, RouterOutlet, LandingPageComponent, HeaderComponent, 
-    NavComponent, FooterComponent, ProdCadastroComponent, ProdAddComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
